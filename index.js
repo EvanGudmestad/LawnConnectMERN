8fd1816342841fd97482fc1cd8dc0ba6ede2da86
EvanGudmestad <eagudmestad@ranken.edu>
2025-11-13 13:47:51 -0600

    Added React with TS, Tailwind and functional login form

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin: ["http://localhost:5173", "http://localhost:3000", "http://localhost:8080", "https://lawnconnect-service-294349793000.us-central1.run.app"],
   credentials: true
 })); 
-app.use(express.static('frontend/dist'));
+app.use(express.static('vite-project/dist'));
 
 // Add better-auth handler - this handles all auth endpoints including cookies
 //Login: POST /api/auth/sign-in/email
---
+++ vite-project/src/App.tsx
@@ -0,0 +1,31 @@
+
+import './App.css'
+import { LoginForm } from '@/components/login-form'
+import { authClient } from '@/lib/auth-client'
+import { Button } from './components/ui/button';
+function App() {
+    const { data: session, isPending } = authClient.useSession();
+
+  if (isPending) {
+    return <div>Loading...</div>;
+  }
+
+  
+  if (!session) {
+    return (
+      <div className="flex min-h-svh items-center justify-center px-4">
+        <div className="w-full max-w-md">
+          <LoginForm />
+        </div>
+      </div>
+    );
+  }
+ return (
+    <div>
+      <h1>Welcome, {session.user.email}</h1>
+      <Button variant="default" onClick={() => authClient.signOut()}>Logout</Button>
+    </div>
+  );
+}
+
+export default App
